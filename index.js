81deea44b65a5df32bf99ad391308cc808e2cda8
abdulle12 <146827203+abdulle12@users.noreply.github.com>
2023-10-16 14:34:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-const names = ["John", "Alice", "Bob", "Jasmine", "Michael"];
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
 
-// Loop through the array
-for (let i = 0; i < names.length; i++) {
-    // Get the current name
-    const name = names[i];
-
-    // Check if the name starts with 'j' or 'J'
-    if (name.charAt(0) === 'j' || name.charAt(0) === 'J') {
-        console.log("Goodbye " + name);
-    } else {
-        console.log("Hello " + name);
-    }
-}
-
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
